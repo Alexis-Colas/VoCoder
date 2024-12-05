06ff478487581cc52a6a979e525fba45e856d59a
Suzdeloffre <suzanne.deloffre@edu.univ-fcomte.fr>
2024-12-05 14:06:46 +0100

    dépot

--- Pauvocoder.java
+++ Pauvocoder.java
@@ -1,67 +1,123 @@
+import static java.lang.System.exit;
+
 public class Pauvocoder {
 
+    // Processing SEQUENCE size (100 msec with 44100Hz samplerate)
+    final static int SEQUENCE = StdAudio.SAMPLE_RATE/10;
+
+    // Overlapping size (20 msec)
+    final static int OVERLAP = SEQUENCE/5 ;
+    // Best OVERLAP offset seeking window (15 msec)
+    final static int SEEK_WINDOW = 3*OVERLAP/4;
+
+    public static void main(String[] args) {
+        if (args.length < 2)
+        {
+            System.out.println("usage: pauvocoder <input.wav> <freqScale>\n");
+            exit(1);
+        }
+
+
+        String wavInFile = args[0];
+        double freqScale = Double.valueOf(args[1]);
+        String outPutFile= wavInFile.split("\\.")[0] + "_" + freqScale +"_";
+
+        // Open input .wev file
+        double[] inputWav = StdAudio.read(wavInFile);
+
+        // Resample test
+        double[] newPitchWav = resample(inputWav, freqScale);
+        StdAudio.save(outPutFile+"Resampled.wav", newPitchWav);
+
+        // Simple dilatation
+        double[] outputWav   = vocodeSimple(newPitchWav, 1.0/freqScale);
+        StdAudio.save(outPutFile+"Simple.wav", outputWav);
+
+        // Simple dilatation with overlaping
+        outputWav = vocodeSimpleOver(newPitchWav, 1.0/freqScale);
+        StdAudio.save(outPutFile+"SimpleOver.wav", outputWav);
+
+        // Simple dilatation with overlaping and maximum cross correlation search
+        outputWav = vocodeSimpleOverCross(newPitchWav, 1.0/freqScale);
+        StdAudio.save(outPutFile+"SimpleOverCross.wav", outputWav);
+
+        joue(outputWav);
+
+        // Some echo above all
+        outputWav = echo(outputWav, 100, 0.7);
+        StdAudio.save(outPutFile+"SimpleOverCrossEcho.wav", outputWav);
+
+        // Display waveform
+        displayWaveform(outputWav);
+
+    }
+
     /**
-     * ré-echantillonner le signal audio
-     * @param input
+     * Resample inputWav with freqScale
+     * @param inputWav
      * @param freqScale
-     * @return
+     * @return resampled wav
      */
-    public static double[] resample(double[] input, double freqScale){
-
+    public static double[] resample(double[] inputWav, double freqScale) {
+        throw new UnsupportedOperationException("Not implemented yet");
     }
 
     /**
-     *ajoute un echo au signal d'entrée
-     * @param input
-     * @param delayMs
-     * @param attn
-     * @return
+     * Simple dilatation, without any overlapping
+     * @param inputWav
+     * @param dilatation factor
+     * @return dilated wav
      */
-    public static double[] echo(double[] input, double delayMs, double attn){
-
+    public static double[] vocodeSimple(double[] inputWav, double dilatation) {
+        throw new UnsupportedOperationException("Not implemented yet");
     }
 
     /**
-     *impémente une dilatation temporeille dans une vesion minimal
-     * @param input
-     * @param timeScale
-     * @return
+     * Simple dilatation, with overlapping
+     * @param inputWav
+     * @param dilatation factor
+     * @return dilated wav
      */
-    public static double[] vocodeSimple(double[] input, double timeScale){
-
+    public static double[] vocodeSimpleOver(double[] inputWav, double dilatation) {
+        throw new UnsupportedOperationException("Not implemented yet");
     }
 
     /**
-     *dilatation temporelle sans clics
-     * @param input
-     * @param timeScale
-     * @return
+     * Simple dilatation, with overlapping and maximum cross correlation search
+     * @param inputWav
+     * @param dilatation factor
+     * @return dilated wav
      */
-    public static double[] vocodeSimpleOver(double[] input, double timeScale){
-
+    public static double[] vocodeSimpleOverCross(double[] inputWav, double dilatation) {
+        throw new UnsupportedOperationException("Not implemented yet");
     }
 
     /**
-     * dilatation temporelle sans clics et avec le maximum de coorélation
-     * @param input
-     * @param timeScale
-     * @return
+     * Play the wav
+     * @param wav
      */
-    public static double[] vocodeSimpleOverCross(double[] input, double timeScale){
-
+    public static void joue(double[] wav) {
+        throw new UnsupportedOperationException("Not implemented yet");
     }
 
     /**
-     *envoie la sortie audio les données contenues dans le tableau input
-     * et produit un affichage des echantillons jouées en temps réel
-     * @param input
+     * Add an echo to the wav
+     * @param wav
+     * @param delay in msec
+     * @param gain
+     * @return wav with echo
      */
-    public static void joue(double[] input){
-
+    public static double[] echo(double[] wav, double delay, double gain) {
+        throw new UnsupportedOperationException("Not implemented yet");
     }
 
-    public static void main(Strings[] agrs){
-
+    /**
+     * Display the waveform
+     * @param wav
+     */
+    public static void displayWaveform(double[] wav) {
+        throw new UnsupportedOperationException("Not implemented yet");
     }
+
 
 }
