9de4cfe83a4f565a66ea19c8ac6826daeb9c1afa
Alexis-Colas <alexis.colas@edu.unviv-fcomte.fr>
2024-12-05 13:56:59 +0100

    ajout methode

--- Pauvocoder.java
+++ Pauvocoder.java
@@ -1,4 +1,27 @@
 public class Pauvocoder {
 
+    public static double[] resample(double[] input, double freqScale){
+
+    }
+
+    public static double[] echo(double[] input, double delayMs, double attn){
+
+    }
+
+    public static double[] vocodeSimple(double[] input, double timeScale){
+
+    }
+
+    public static double[] vocodeSimpleOver(double[] input, double timeScale){
+
+    }
+
+    public static double[] vocodeSimpleOverCross(double[] input, double timeScale){
+
+    }
+
+    public static void joue(double[] input){
+
+    }
 
 }
