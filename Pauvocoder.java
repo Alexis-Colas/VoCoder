90284e617ee9f4ad6c899e3d7d9dd74b6c804039
Suzdeloffre <suzanne.deloffre@edu.univ-fcomte.fr>
2024-12-05 13:59:50 +0100

    commentaires et main

--- Pauvocoder.java
+++ Pauvocoder.java
@@ -1,26 +1,65 @@
 public class Pauvocoder {
 
+    /**
+     *
+     * @param input
+     * @param freqScale
+     * @return
+     */
     public static double[] resample(double[] input, double freqScale){
 
     }
 
+    /**
+     *
+     * @param input
+     * @param delayMs
+     * @param attn
+     * @return
+     */
     public static double[] echo(double[] input, double delayMs, double attn){
 
     }
 
+    /**
+     *
+     * @param input
+     * @param timeScale
+     * @return
+     */
     public static double[] vocodeSimple(double[] input, double timeScale){
 
     }
 
+    /**
+     *
+     * @param input
+     * @param timeScale
+     * @return
+     */
     public static double[] vocodeSimpleOver(double[] input, double timeScale){
 
     }
 
+    /**
+     *
+     * @param input
+     * @param timeScale
+     * @return
+     */
     public static double[] vocodeSimpleOverCross(double[] input, double timeScale){
 
     }
 
+    /**
+     *
+     * @param input
+     */
     public static void joue(double[] input){
+
+    }
+
+    public static void main(Strings[] agrs){
 
     }
 
